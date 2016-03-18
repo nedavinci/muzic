71993f1a0138be48f2b247cef308488ff70f34ea
Павел Грибанов <rapthead@gmail.com>
2016-03-18 23:38:43 +0500

    STATIC_URL fix

--- musiclib/settings/test.py
+++ musiclib/settings/test.py
@@ -7,7 +7,7 @@ FORCE_SCRIPT_NAME = '/musiclib'
 MUSIC_LIBRARY_PATH = '/home/music/lossless/'
 
 MUSIC_LIBRARY_BASE_URL = FORCE_SCRIPT_NAME + MUSIC_LIBRARY_BASE_URL
-STATIC_URL = FORCE_SCRIPT_NAME + '/static'
+STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
