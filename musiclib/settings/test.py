3113b701fd102a91070beeeb5d5900a6700974f6
Pavel Gribanov <rapthead@gmail.com>
2016-04-09 15:37:50 +0500

    settings update

--- musiclib/settings/test.py
+++ musiclib/settings/test.py
@@ -13,4 +13,20 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
-INSTALLED_APPS.extend(('debug_toolbar', 'django_extensions'))
+INSTALLED_APPS.extend(('debug_toolbar', 'django_extensions', 'template_timings_panel'))
+INTERNAL_IPS = ('192.168.154.60',)
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
