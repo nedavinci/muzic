9df0c6a6e750a749c899b8a7d445af7a1aa6b003
Павел Грибанов <rapthead@gmail.com>
2016-04-13 01:48:04 +0500

    template caching enable

--- musiclib/settings/production.py
+++ musiclib/settings/production.py
@@ -22,3 +22,25 @@ CACHEOPS_REDIS = {
 CACHEOPS = {
     'musiclib.*': {'ops': 'all', 'timeout': 60*60},
 }
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'OPTIONS': {
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
