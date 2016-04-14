e31b5f85390eeee820ebc908deb0741037cb0f8c
Pavel Gribanov <rapthead@gmail.com>
2016-04-14 23:26:26 +0500

    production settings update

--- musiclib/settings/production.py
+++ musiclib/settings/production.py
@@ -10,18 +10,18 @@ MUSIC_LIBRARY_BASE_URL = FORCE_SCRIPT_NAME + MUSIC_LIBRARY_BASE_URL
 STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 ALLOWED_HOSTS = ["*"]
 
-INSTALLED_APPS.extend(('cacheops',))
-
-CACHEOPS_REDIS = {
-    'host': 'localhost',
-    'port': 6379,       
-    'db': 1,            
-    'socket_timeout': 3,
-}
-
-CACHEOPS = {
-    'musiclib.*': {'ops': 'all', 'timeout': 60*60},
-}
+# INSTALLED_APPS.extend(('cacheops',))
+# 
+# CACHEOPS_REDIS = {
+#     'host': 'localhost',
+#     'port': 6379,       
+#     'db': 1,            
+#     'socket_timeout': 3,
+# }
+# 
+# CACHEOPS = {
+#     'musiclib.*': {'ops': 'all', 'timeout': 60*60},
+# }
 
 TEMPLATES = [
     {
@@ -44,3 +44,21 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/nnm/projects/musiclib/run/django.errors.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
